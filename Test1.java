7bddef8ae097d82f79764002d8c0506bed6d1f95
ranjan1122 <rashmi.rrp@gmail.com>
2025-02-01 17:39:49 +0530

    commit-2

--- Test1.java
+++ Test1.java
@@ -2,6 +2,7 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hi All..how are you doing All!!");
         System.out.println("I am changing something for testing purpose!!");
+        System.out.println("let do some text insertion");
     }
     
 }
